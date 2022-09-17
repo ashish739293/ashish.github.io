50a094c434a33af6200048fe660ef32506d9a181
ashish739293 <108934924+ashish739293@users.noreply.github.com>
2022-09-17 11:13:18 -0700

    my wish

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Your Name");
+ 
+  	if(a== "nishu"||a=="Nishu")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
